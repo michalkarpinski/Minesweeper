253467e0f1c291bdf19b68dacff66b966565c663
Michał <Micha>
2022-01-30 00:33:39 +0100

    add mines left count

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,12 @@ const NUMER_OF_MINES = 10;
 
 const board = createBoard(BOARD_SIZE, NUMER_OF_MINES);
 const boardElement = document.querySelector('.board');
-boardElement.style.setProperty('--size', BOARD_SIZE);
-
-console.log(board);
+const minesLeftText = document.querySelector('[data-mine-count]');
 
 board.forEach(row => {
     row.forEach(tile => {
         boardElement.append(tile.element);
     })
 })
+boardElement.style.setProperty('--size', BOARD_SIZE);
+minesLeftText.textContent = NUMER_OF_MINES;
