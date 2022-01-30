ab2642e543563700983d38870673053c6b4b4697
Michał <Micha>
2022-01-30 01:06:46 +0100

    add prettier extension

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,23 @@
-import {
-    createBoard
-} from "./minesweeper.js";
+import { createBoard } from "./minesweeper.js";
 
 const BOARD_SIZE = 10;
 const NUMER_OF_MINES = 10;
 
 const board = createBoard(BOARD_SIZE, NUMER_OF_MINES);
-const boardElement = document.querySelector('.board');
-const minesLeftText = document.querySelector('[data-mine-count]');
+const boardElement = document.querySelector(".board");
+const minesLeftText = document.querySelector("[data-mine-count]");
 
-board.forEach(row => {
-    row.forEach(tile => {
-        boardElement.append(tile.element);
-    })
-})
-boardElement.style.setProperty('--size', BOARD_SIZE);
+board.forEach((row) => {
+  row.forEach((tile) => {
+    boardElement.append(tile.element);
+
+    tile.element.addEventListener("click", () => {});
+
+    tile.element.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      markTile(tile);
+    });
+  });
+});
+boardElement.style.setProperty("--size", BOARD_SIZE);
 minesLeftText.textContent = NUMER_OF_MINES;
